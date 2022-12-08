987c6254b85f953c12c6bcc0f42e0ab9411b3141
Han Gao <zasdivc@gmail.com>
2022-12-07 21:11:58 -0500

    Update LoadSML2DB.LuHanW_HanG.R

--- LoadSML2DB.LuHanW_HanG.R
+++ LoadSML2DB.LuHanW_HanG.R
@@ -21,6 +21,8 @@ dbSendQuery(mydb, "drop table if exists ArticleAuthor")
 dbSendQuery(mydb, "CREATE TABLE Journals (
             JournalID TEXT NOT NULL PRIMARY KEY,
             ISSN TEXT,
+            
+            
             Year int,
             Month int,
             Title TEXT,
@@ -116,6 +118,29 @@ keyExists <- function (aKey, aDF)
   return(0)
 }
 
+rowExists <- function (aRow, aDF)
+{
+  n <- nrow(aDF)
+  if (n == 0)
+  {
+    # data frame is empty, so can't exist
+    return(0)
+  }
+  
+  for (a in 1:n)
+  {
+    # check if all columns match for a row; ignore the aID column
+    if (all(aDF[a,] == aRow[1,]))
+    {
+      # found a match; return it's ID
+      return(a)
+    }
+  }
+  
+  # none matched
+  return(0)
+}
+
 p <- function(..., sep='-') {
   paste(..., sep=sep, collapse=sep)
 }
@@ -134,39 +159,79 @@ LastNameGroup <- xpathSApply(xmlDOM, "//LastName",xmlValue)
 ForeNameGroup <- xpathSApply(xmlDOM, "//ForeName",xmlValue)
 InitialsGroup <- xpathSApply(xmlDOM, "//Initials",xmlValue)
 TitleGroup <- xpathSApply(xmlDOM, "//Title",xmlValue)
+JournalGroup <- xpathSApply(xmlDOM, "//Journal",xmlValue)
+
+
+print(length(LanguageGroup))
+print(length(JournalGroup))
+print(length(ArticleTitleGroup))
+print(length(ISSNGroup))
+print(length(VolumeGroup))
+print(length(IssueGroup))
+print(length(PubDateGroup))
+print(length(ISOAbbreviationGroup))
+print(length(LastNameGroup))
+print(length(ForeNameGroup))
+print(length(InitialsGroup))
+print(length(TitleGroup))
+print(length(numPA))
+
 
 AuthorCount = 1
 AuthorListCount = 1
 JournalCount = 1
 
-print(length(ISSNGroup))
-print(length(TitleGroup))
 
 for (i in 1:numPA)
 {
   # get next PubmedArticle node
   aPA <- r[[i]]
   PMID <- xmlAttrs(aPA)
-  # Extract PMID
-  # PMID <- as.numeric(a[1])
-  
+  if (length(PMID) == 0) {
+    PMID <- 0
+  }
+  print(aPA)
+
   # get next Article node
   aAT <- aPA[[1]]
-  Language <- LanguageGroup[[i]]
-  ArticleTitle <- ArticleTitleGroup[[i]]
+
+  Language <- xpathSApply(aAT, "./Language",xmlValue)
+  if (length(Language) != 1) {
+    Language <- "Language"
+  }
+
+  ArticleTitle <- xpathSApply(aAT, "./ArticleTitle",xmlValue)
+  if (length(ArticleTitle) == 0) {
+    ArticleTitle <- "ArticleTitle"
+  }
   
   # get next Journal node
-  aJN <- aAT[[1]]
-  ISSN <- ISSNGroup[[i]]
+  aJN <- xpathSApply(aAT, "./Journal")
   
-  aJournalIssue <- aJN[[2]]
+  ISSN <- xpathSApply(aPA, "./Article/Journal/ISSN",xmlValue)
+  if (length(ISSN) == 0) {
+    ISSN <- "ISSN"
+  }
+  
+  # aJournalIssue <- aJN[[2]]
   # CitedMedium <- xmlAttrs(aJournalIssue)
   #Volume <- VolumeGroup[[i]]
   #Issue <- IssueGroup[[i]]
-  PubDate <- PubDateGroup[[i]]
-  Title <- TitleGroup[[i]]
-  ISOAbbreviation <- ISOAbbreviationGroup[[i]]
-  
+  PubDate <- xpathSApply(aPA, "./Article/Journal/JournalIssue/PubDate",xmlValue)
+  if (length(PubDate) == 0) {
+    PubDate <- "1900JAN"
+  }
+
+  Title <- xpathSApply(aPA, "./Article/Journal/Title",xmlValue)
+  if (length(Title) == 0) {
+    Title <- "Title"
+  }
+
+  ISOAbbreviation <- xpathSApply(aPA, "./Article/Journal/ISOAbbreviation",xmlValue)
+  if (length(ISOAbbreviation) == 0) {
+    ISOAbbreviation <- "ISOAbbreviation"
+  }
+
   # Split PubDate to Year and Month
   spaceCount <- str_count(PubDate, ' ')
   PubDate2 <- PubDate
@@ -181,8 +246,7 @@ for (i in 1:numPA)
     Month <- match(MonthStr,month.abb)
   }
   
-  # get next Author node
-  aAuthorList <- aAT[[4]]
+  
   
   JournalID <- p(ISSN, PubDate)
   
@@ -206,17 +270,31 @@ for (i in 1:numPA)
   df.articles[i,'Language'] <- as.character(Language)
   df.articles[i,'ArticleTitle'] <- as.character(ArticleTitle)
   
-  
-  
+  # get next Author node
+  aAuthorList <- aAT[[4]]
+
   numAuthor <- xmlSize(aAuthorList)
+  if (numAuthor == 0) {
+    next
+  }
   for (j in 1:numAuthor)
   {
     aAuthor <- aAuthorList[[j]]
-    LastName <- LastNameGroup[[AuthorCount]]
-    ForeName <- ForeNameGroup[[AuthorCount]]
+    LastName <- xpathSApply(aAuthor, "./LastName",xmlValue)
+    if (length(LastName) == 0) {
+      LastName <- "LastName"
+    }
+    ForeName <- xpathSApply(aAuthor, "./ForeName",xmlValue)
+    if (length(ForeName) == 0) {
+      ForeName <- "ForeName"
+    }
     # ValidYN <- xmlAttrs(aAuthor)
     AuthorID <- p(ForeName, LastName)
-    Initials <- InitialsGroup[[AuthorCount]]
+    
+    Initials <- xpathSApply(aAuthor, "./Initials",xmlValue)
+    if (length(Initials) == 0) {
+      Initials <- "Initials"
+    }
     
     df.articleauthor[AuthorCount,'PMID'] <- as.character(PMID)
     df.articleauthor[AuthorCount,'AuthorID'] <- as.character(AuthorID)
